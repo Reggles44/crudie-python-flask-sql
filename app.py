0b3243122feead541b0374456d8a3f364167362b
Reggles44 <reginaldbeakes@gmail.com>
2024-02-18 16:58:30 -0500

    init

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase 
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 
 app = Flask(__name__)
 
+
 class Base(DeclarativeBase):
-  pass
+    pass
+
 
 db = SQLAlchemy(model_base=Base)
 db.init_app(app)
+
 
 class PythonFlaskSQLData(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     foo: Mapped[str]
     bar: Mapped[int]
-    
 
-@app.route('/create' , methods = ['POST',])
+
+@app.route(
+    "/create",
+    methods=[
+        "POST",
+    ],
+)
 def create():
     new_data = PythonFlaskSQLData(request.get_json())
     db.session.add(**new_data)
@@ -25,13 +33,23 @@ def create():
     return 200
 
 
-@app.route('/read' , methods = ['GET',])
+@app.route(
+    "/read",
+    methods=[
+        "GET",
+    ],
+)
 def read():
     args_dict = request.args.to_dict()
     return db.one_or_404(db.select(PythonFlaskSQLData).filter_by(**args_dict))
 
 
-@app.route('/update' , methods = ['PATCH',])
+@app.route(
+    "/update",
+    methods=[
+        "PATCH",
+    ],
+)
 def update():
     request_dict = request.get_json()
     data = db.one_or_404(db.select(PythonFlaskSQLData).filter_by(**request_dict))
@@ -40,13 +58,19 @@ def update():
     return 200
 
 
-@app.route('/delete' , methods = ['DELETE',])
+@app.route(
+    "/delete",
+    methods=[
+        "DELETE",
+    ],
+)
 def delete():
     args_dict = request.args.to_dict()
     data = db.one_or_404(db.select(PythonFlaskSQLData).filter_by(**args_dict))
     db.session.delete(data)
     db.session.commit()
     return 200
+
 
 # Create all db tables
 with app.app_context():
